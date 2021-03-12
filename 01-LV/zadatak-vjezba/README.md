0f3e2e306972b8ba097415ab2f7dfb37d546fd9d
Luka <luka@pop-os.localdomain>
2021-03-12 12:27:29 +0100

    add zadatak lv1

---
+++ 01-LV/zadatak-vjezba/README.md
@@ -0,0 +1,7 @@
+# LV1 - Zadatak - Vjezba
+
+Potrebno je otvoriti template-design.png sliku iz odgovarajuće grupe i napraviti sljedeće:
+
+- Kodirati HTML prema navedenom dizajnu za desktop rezoluciju
+- Prilagoditi HTML kod da odgovara SEO značajkama
+- Uključiti ARIA atribute kako bi prilagodili web stranicu čitačima zaslona
---
+++ 01-LV/zadatak-vjezba/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LV1 - zadatak za vježbu</title>
+</head>
+<body>
+    <h1><?= "Hello LV1 practice task"?></h1>
+</body>
+</html>
